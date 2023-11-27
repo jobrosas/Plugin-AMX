6383deb0cdc07230075934acaa4978dd11eed88a
Job Rosas <job.11@hotmail.com>
2023-11-27 02:05:52 -0600

    Re installing WP Block

--- src/edit.js
+++ src/edit.js
@@ -29,7 +29,6 @@ import './editor.scss';
  *
  * @return {Element} Element to render.
  */
-/*
 export default function Edit() {
 	return (
 		<p { ...useBlockProps() }>
@@ -39,72 +38,4 @@ export default function Edit() {
 			) }
 		</p>
 	);
-}
-*/
-
-import { registerBlockType } from '@wordpress/blocks';
-import { TextControl, PanelColor, ColorPalette } from '@wordpress/components';
-import { select } from '@wordpress/data';
-
-registerBlockType('bloque-categorias-colores/bloque-categorias-colores', {
-    title: 'Bloque Categorías Colores',
-    icon: 'smiley',
-    category: 'common',
-    attributes: {
-        title: {
-            type: 'string',
-            default: 'Título predeterminado',
-        },
-        description: {
-            type: 'string',
-            default: 'Descripción predeterminada',
-        },
-    },
-    edit: function (props) {
-        const { title, description } = props.attributes;
-
-        const categories = select('core').getEntityRecords('taxonomy', 'category');
-        const currentPost = select('core/editor').getCurrentPost();
-        const currentCategories = currentPost ? currentPost.categories : [];
-
-        const onChangeTitle = (newTitle) => {
-            props.setAttributes({ title: newTitle });
-        };
-
-        const onChangeDescription = (newDescription) => {
-            props.setAttributes({ description: newDescription });
-        };
-
-        return (
-            <div {...useBlockProps()}>
-                <TextControl label="Título" value={title} onChange={onChangeTitle} />
-                <TextControl label="Descripción" value={description} onChange={onChangeDescription} />
-
-                {categories && (
-                    <PanelColor title="Categoría" colorValue={getCategoryColor(currentCategories)}>
-                        <ColorPalette
-                            colors={categories.map((category) => ({
-                                name: category.name,
-                                color: getCategoryColor([category.id]),
-                            }))}
-                            onChange={(color) => applyCategoryStyle(color, props.clientId)}
-                        />
-                    </PanelColor>
-                )}
-            </div>
-        );
-    },
-    save: function () {
-        // Guardar contenido aquí
-    },
-});
-
-function getCategoryColor(categoryIds) {
-    // Lógica para obtener el color de la categoría seleccionada
-    // ...
-}
-
-function applyCategoryStyle(color, clientId) {
-    // Lógica para aplicar el estilo de la categoría seleccionada al bloque
-    // ...
 }
