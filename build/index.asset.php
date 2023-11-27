6d9a580dba259e8a181152139668db842b033240
Job Rosas <job.11@hotmail.com>
2023-11-27 02:08:38 -0600

    Second version of plugin code block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '79681256541d51d41ab8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => 'dff6956f39641ccce25d');
